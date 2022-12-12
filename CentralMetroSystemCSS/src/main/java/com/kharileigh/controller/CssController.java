913992e121c4dcdb2489718a520d959618bf1887
kharileigh <kharileighmiller@gmail.com>
2022-12-12 11:01:19 +0000

    SWIPE IN - dropdown completed

--- CentralMetroSystemCSS/src/main/java/com/kharileigh/controller/CssController.java
+++ CentralMetroSystemCSS/src/main/java/com/kharileigh/controller/CssController.java
@@ -5,7 +5,11 @@
 
 package com.kharileigh.controller;
 
+import com.kharileigh.entity.Station;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,7 +21,7 @@ public class CssController {
     @RequestMapping("/")
     public ModelAndView landingPageController() {
     
-        return new ModelAndView("homepage");
+        return new ModelAndView("LandingPage");
     }
     
     
@@ -34,5 +38,34 @@ public class CssController {
     public ModelAndView newUserPageController() {
     
         return new ModelAndView("NewUser");
+    }
+    
+    
+    // HOMEPAGE
+    @RequestMapping("/homepage")
+    public ModelAndView homepageController() {
+    
+        return new ModelAndView("HomePage");
+    }
+    
+     // SWIPES IN PAGE
+    @RequestMapping("/swipesInPage")
+    public ModelAndView swipesInPageController() {
+    
+        return new ModelAndView("SwipesInPage");
+    }
+    
+    // LISTS ALL THE STATIONS
+    @ModelAttribute("stations")
+    public List <Station> getStation() {
+        List<Station> list = new ArrayList<Station>();
+        list.add(new Station(1, "Bank"));
+        list.add(new Station(2, "Lewisham"));
+        list.add(new Station(3, "Croydon"));
+        list.add(new Station(4, "Harlow"));
+        list.add(new Station(5, "Euston"));
+        
+        return list;
+        
     }
 }
---
+++ CentralMetroSystemCSS/src/main/java/com/kharileigh/entity/Station.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author kharileigh
+ */
+
+package com.kharileigh.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Station {
+    
+    private int stationId;
+    private String stationName;
+}
